a9579e4fd8f2f6b67ff55d29be851f46b61e2a4d
Isak Sky <isak.sky@gmail.com>
2021-02-25 11:43:18 -0700

    Initial commit, reformat-sql working

---
+++ PodHandler.cs
@@ -0,0 +1,172 @@
+﻿using BencodeNET.Objects;
+using BencodeNET.Parsing;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipelines;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace pod.xledger.tsql_scriptdom {
+    public class PodHandler {
+        PipeReader _reader;
+        PipeWriter _writer;
+        BencodeParser _parser;
+
+        public PodHandler(Stream inputStream, Stream outputStream) {
+            _reader = PipeReader.Create(inputStream);
+            _writer = PipeWriter.Create(outputStream, new StreamPipeWriterOptions(leaveOpen: true));
+            _parser = new BencodeParser();
+        }
+
+        public async Task HandleMessages() {
+            var cts = new CancellationTokenSource();
+            while (!cts.IsCancellationRequested) {
+                try {
+                    var msg = await _parser.ParseAsync<BDictionary>(_reader, cts.Token);
+                    if (msg.TryGetValue("op", out IBObject op)) {
+                        var s = ((BString)op).ToString();
+                        await HandleMessage(s, msg, cts);
+                    }
+                } catch (OperationCanceledException) {
+                } catch (Exception ex) {
+                    await SendException(null, ex.Message);
+                }
+            }
+        }
+
+        async Task HandleMessage(string operation, BDictionary msg, CancellationTokenSource cts) {
+            switch (operation) {
+                case "describe":
+                    var resp = new BDictionary {
+                        ["format"] = new BString("json"),
+                        ["namespaces"] = new BList {
+                            new BDictionary {
+                                ["name"] = new BString("pod.xledger.tsql-scriptdom"),
+                                ["vars"] = new BList {
+                                    new BDictionary { ["name"] = new BString("reformat-sql") }
+                                }
+                             }
+                        },
+                        ["ops"] = new BDictionary {
+                            ["shutdown"] = new BDictionary()
+                        }
+                    };
+                    await resp.EncodeToAsync(_writer);
+                    break;
+                case "shutdown":
+                    cts.Cancel();
+                    break;
+                case "invoke":
+                    await HandleInvoke(msg, cts);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static string JSON(object o) {
+            var s = JsonConvert.SerializeObject(o);
+            return s;
+        }
+
+        public static JToken ParseJson(string s) {
+            var reader = new JsonTextReader(new StringReader(s));
+
+            // We don't need/want NewtonSoft to tamper with our data:
+            reader.DateParseHandling = DateParseHandling.None;
+            reader.DateTimeZoneHandling = DateTimeZoneHandling.RoundtripKind;
+
+            return JToken.Load(reader);
+        }
+
+        public static class StatusMessages {
+            public static readonly BList DONE_ERROR = new BList(new[] { "done", "error" });
+
+            public static readonly BList DONE = new BList(new[] { "done" });
+        }
+
+        async Task SendException(string id, string exMessage, object exData = null) {
+            var resp = new BDictionary {
+                ["ex-message"] = new BString(exMessage),
+                ["status"] = StatusMessages.DONE_ERROR
+            };
+            if (id != null) { resp["id"] = new BString(id); }
+            if (exData != null) { resp["ex-data"] = new BString(JSON(exData)); }
+            await resp.EncodeToAsync(_writer);
+        }
+
+        async Task SendResult(string id, object result, bool isJson = false) {
+            var json = isJson ? (string)result : JSON(result);
+            var resp = new BDictionary {
+                ["id"] = new BString(id),
+                ["value"] = new BString(json),
+                ["status"] = StatusMessages.DONE
+            };
+            await resp.EncodeToAsync(_writer);
+        }
+
+        async Task HandleInvoke(BDictionary msg, CancellationTokenSource cts) {
+            if (!(msg.TryGetNonBlankString("id", out var id)
+                && msg.TryGetNonBlankString("var", out var varname))) {
+                await SendException(id, "Missing \"id\" and/or \"var\" keys in \"invoke\" operation payload");
+                return;
+            }
+
+            switch (varname) {
+                case "pod.xledger.tsql-scriptdom/reformat-sql":
+                    await HandleVar_Reformat(id, msg);
+                    break;
+                default:
+                    await SendException(id, $"No such var: \"{varname}\"");
+                    break;
+            }
+        }
+
+        async Task HandleVar_Reformat(string id, BDictionary msg) {
+            if (!msg.TryGetValue("args", out var beArgs) || !(beArgs is BString beArgsStr)) {
+                await SendException(id, $"Missing required \"args\" argument.");
+                return;
+            }
+
+            IReadOnlyDictionary<string, JToken> argMap;
+            try {
+                argMap = JsonConvert.DeserializeObject<IList<IReadOnlyDictionary<string, JToken>>>(beArgsStr.ToString()).First();
+            } catch (Exception ex) {
+                await SendException(id, $"Couldn't deserialize json payload. Expected a map. Error: {ex.Message}");
+                return;
+            }
+
+            if (!argMap.TryGetNonBlankString("sql", out var sql)) {
+                await SendException(id, $"Missing required \"sql\" argument.");
+                return;
+            }
+
+            argMap.TryGetBool("initial-quoted-identifiers", out var initialQuotedIdentifiers);
+
+            var parser = new TSql150Parser(initialQuotedIdentifiers);
+            var fragment = parser.Parse(new StringReader(sql), out var errors);
+
+            if (errors.Count > 0) {
+                await SendException(id, "Could not parse sql", new Dictionary<string, object> { ["errors"] = errors });
+                return;
+            }
+
+            var scriptGen = new Sql150ScriptGenerator();
+            scriptGen.GenerateScript(fragment, out string formattedSql);
+
+            await SendResult(id, formattedSql);
+        }
+    }
+
+    public class ResultSet {
+        public string[] columns;
+        public List<object[]> rows;
+    }
+}
---
+++ README.md
@@ -0,0 +1,30 @@
+﻿# pod.xledger.tsql-scriptdom
+
+[babashka](https://github.com/borkdude/babashka) [pod](https://github.com/babashka/babashka.pods) for using [Microsoft.SqlServer.TransactSql.ScriptDom]https://docs.microsoft.com/en-us/dotnet/api/microsoft.sqlserver.transactsql.scriptdom?view=sql-dacfx-150, a TSQL parsing / script generation library.
+
+## Usage
+
+```clojure
+(require '[babashka.pods :as pods])
+
+;; After compiling this solution with Visual Studio:
+(pods/load-pod "C:/src/pod_tsql_scriptdom/bin/Debug/net5.0/pod.xledger.tsql_scriptdom.exe")
+;; or, if you are not on Windows:
+(pods/load-pod ["dotnet" "bin/Debug/net5.0/pod.xledger.tsql_scriptdom.dll"])
+
+(require '[pod.xledger.tsql-scriptdom :as tsql-scriptdom])
+
+(println (tsql-scriptdom/reformat-sql {
+                                         :sql "select [name], (select [name], [max_length] from sys.parameters b where b.object_id = a.object_id for json path) as params from sys.procedures a for json path"
+                                         :initial-quoted-identifiers false ;; the default   
+                                         }))
+SELECT [name],
+       (SELECT [name],
+               [max_length]
+        FROM   sys.parameters AS b
+        WHERE  b.object_id = a.object_id
+        FOR    JSON PATH) AS params
+FROM   sys.procedures AS a
+FOR    JSON PATH;
+
+=> nil
